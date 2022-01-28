f19fc67746b18528ff7f0ad38aa850afb8cb0059
高島空良 <takashimasora@takashimakuuryounoMacBook-Pro.local>
2022-01-29 02:43:52 +0900

    update command

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import subprocess
+subprocess.run('python -m unidic download')
 from fastapi import FastAPI, Form, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
