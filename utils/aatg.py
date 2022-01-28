8af0b3ff50581c1fc24b3c3582f8ce47158d9c35
高島空良 <takashimasora@takashimakuuryounoMacBook-Pro.local>
2022-01-29 04:22:23 +0900

    yamane www

--- utils/aatg.py
+++ utils/aatg.py
@@ -4,6 +4,7 @@ import unidic_lite
 import pykakasi
 from alphabet2kana import a2k
 import warnings
+import string
 warnings.simplefilter('ignore')
 
 class AATG():
@@ -14,12 +15,15 @@ class AATG():
     kakasi.setMode('K', 'a')
     kakasi.setMode('J', 'a')
 
-    self.vowel2Ingo = {'an':["マン"],'anii': ['3P'], 'eeuiouu': ['AV女優'], 'iiuoto': ['Gスポット'], 'eoae': ['NTR', '寝取られ'], 'etuu': ['SEX', 'せっくす', 'セックス'], 'eueu': ['SM'], 'euooii': ['SOD'], 'iiatu': ['Tバック'], 'iaaii': ['いやらしい'], 'eti': ['えっち', 'エッチ'], 'oinin': ['おちんちん'], 'otai': ['おっπ', 'おっぱい'], 'oaii': ['おなにー', 'オナニー'], 'oeoa': ['おねショタ'], 'ooo': ['おぼこ'], 'oano': ['おまんこ'], 'oeo': ['おめこ'], 'oouiea': ['お掃除フェラ'], 'inaa': ['きんたま', 'キンタマ', '金玉'], 'aaauuoi': ['さかさ椋鳥'], 'ioiuou': ['しぼり芙蓉'], 'uee': ['すけべ', 'スケベ'], 'eieione': ['せきれい本手'], 'aiuioouo': ['だいしゅきホールド'], 'ino': ['ちんこ', 'ちんぽ', 'インポ', 'チンコ', 'チンポ', '淫語'], 'inin': ['ちんちん', 'チンチン', '陰唇', 'ﾁﾝﾁﾝ'], 'ioieti': ['ひとりえっち', '一人Ｈ'], 'uaai': ['ふたなり'], 'anuiaei': ['まんぐり返し'], 'ano': ['まんこ', 'マンコ'], 'anan': ['まんまん'], 'uaua': ['むらむら', 'ムラムラ'], 'aue': ['アクメ'], 'aean': ['アゲマン', 'サゲマン'], 'aauoieo': ['アダルトビデオ'], 'aaii': ['アナニー'], 'aau': ['アナル'], 'aauetuu': ['アナルセックス', 'アナルＳＥＸ'], 'aauiiu': ['アナルビーズ'], 'aauuau': ['アナルプラグ'], 'aauauou': ['アナル拡張'], 'aauaiau': ['アナル開発'], 'aeao': ['アヘ顔'], 'iu': ['イク'], 'iiou': ['イチモツ'], 'iaiaetuu': ['イチャイチャセックス'], 'iaauetuu': ['イチャラブセックス'], 'ieua': ['イメクラ'], 'ieeiieo': ['イメージビデオ'], 'iaaio': ['イラマチオ'], 'inoenu': ['インポテンツ'], 'euuaii': ['エクスタシー'], 'io': ['痴女'], 'eoi': ['エロい', '寝取り', '手コキ'], 'eoouin': ['エロ同人'], 'eoouini': ['エロ同人誌'], 'eoon': ['エロ本'], 'oae': ['オナペ'], 'oaeto': ['オナペット'], 'oao': ['オナホ'], 'oaoou': ['オナホール'], 'ooauu': ['オーガズム'], 'auaa': ['カウパー'], 'anonouei': ['カントン包茎'], 'auoou': ['ギャグボール', 'ラブドール'], 'uuo': ['クスコ', '熟女'], 'uoai': ['クソガキ'], 'uioiu': ['クリトリス'], 'uniinuu': ['クンニリングス'], 'uni': ['クンニ'], 'euano': ['ケツマンコ'], 'onoou': ['コンドーム'], 'aaen': ['ザーメン'], 'ituuain': ['シックスナイン'], 'oaoe': ['ショタおね'], 'uaoo': ['スカトロ'], 'ueeiu': ['スケベ椅子'], 'ueua': ['スペルマ'], 'uatinu': ['スワッピング'], 'eue': ['セフレ'], 'enui': ['センズリ'], 'ouooneano': ['ソフト・オン・デマンド'], 'oouano': ['ソープランド'], 'oouou': ['ソープ嬢'], 'atiaiu': ['ダッチワイフ'], 'auuiiu': ['ダブルピース'], 'iuo': ['ディルド'], 'iiuuooo': ['ディープスロート'], 'eain': ['デカチン'], 'eiaiieuu': ['デリバリーヘルス'], 'eieu': ['デリヘル'], 'ooao': ['トロ顔'], 'ana': ['ナンパ', '顔射'], 'ooan': ['ノーパン'], 'aeoi': ['ハメ撮り'], 'aaeu': ['ハーレム'], 'aiaua': ['バイアグラ'], 'auuuea': ['バキュームフェラ'], 'aiui': ['パイズリ'], 'aian': ['パイパン', 'ヤリマン'], 'aaau': ['パパ活'], 'ania': ['パンチラ'], 'iti': ['ビッチ'], 'iuoatu': ['フィストファック'], 'ea': ['フェラ'], 'eaio': ['フェラチオ'], 'eaui': ['フェラ抜き'], 'uuea': ['ブルセラ'], 'etinu': ['ペッティング'], 'eian': ['ペニバン', '性感'], 'oeu': ['ホ別'], 'oeaa': ['ボテ腹'], 'ooin': ['ポコチン'], 'ouio': ['ポルチオ'], 'auaaeeon': ['マスターベーション'], 'aiin': ['ヤリチン', '催眠', '愛人'], 'auo': ['ラブホ'], 'auoeu': ['ラブホテル'], 'iue': ['リフレ'], 'eiu': ['レイプ'], 'oion': ['ロリコン'], 'aaai': ['中出し', '朝勃ち', '朝起ち'], 'ouai': ['乙π','おっぱい'], 'iaeoan': ['乱れ牡丹'], 'anou': ['乱交', '男娼'], 'iua': ['乳房'], 'iui': ['乳首'], 'itouiai': ['亀甲縛り'], 'iou': ['亀頭', '遅漏'], 'iaa': ['二穴'], 'iaaoui': ['二穴同時'], 'aeiouei': ['仮性包茎'], 'aii': ['体位'], 'oinauei': ['個人撮影'], 'auoaae': ['兜合わせ'], 'iiueone': ['入船本手'], 'enou': ['円光', '援交'], 'oo': ['処女'], 'ouei': ['包茎', '童貞'], 'ouaiaei': ['口内射精'], 'ouaiata': ['口内発射'], 'aauaiauu': ['唐草居茶臼'], 'aeioe': ['喘ぎ声'], 'iuuate': ['四十八手'], 'uooooi': ['太ももコキ'], 'ieaie': ['姫始め'], 'oiau': ['媚薬'], 'aaae': ['孕ませ', '玉舐め', '生ハメ'], 'oouione': ['寿本手'], 'aei': ['射精'], 'ian': ['屍姦'], 'ouu': ['巨乳'], 'oii': ['巨尻'], 'oon': ['巨根'], 'oaeauu': ['帆かけ茶臼'], 'ai': ['座位'], 'ouan': ['強姦', '睾丸'], 'ouaii': ['後背位'], 'iuu': ['微乳'], 'ioiiauu': ['忍び居茶臼'], 'aiauoi': ['快楽堕ち'], 'eiou': ['性交', '性欲'], 'eioi': ['性処理'], 'eioei': ['性奴隷'], 'eianataai': ['性感マッサージ'], 'eianai': ['性感帯'], 'eioui': ['性行為', '正常位'], 'aiu': ['愛撫'], 'aiei': ['愛液'], 'eiinue': ['成人向け'], 'aaniu': ['我慢汁'], 'ean': ['手マン'], 'uin': ['手淫'], 'aiiou': ['抱き地蔵'], 'aeaone': ['揚羽本手'], 'enoouai': ['援助交際'], 'ouou': ['放尿', '早漏', '陵辱'], 'ouiuei': ['放置プレイ'], 'iueauu': ['時雨茶臼'], 'uiiauu': ['月見茶臼'], 'auauui': ['松葉崩し'], 'aaoiauu': ['機織茶臼'], 'iuanuu': ['汁男優'], 'aieu': ['泡姫'], 'oaiione': ['洞入り本手'], 'inan': ['淫乱', '輪姦'], 'inou': ['淫行', '陰嚢', '陰毛', '飲尿'], 'ini': ['淫靡'], 'auuu': ['爆乳'], 'uuan': ['獣姦'], 'auou': ['発情'], 'ineiouei': ['真性包茎'], 'uian': ['睡姦'], 'aeue': ['種付け'], 'aeueueu': ['種付けプレス'], 'aaouai': ['穴兄弟'], 'aino': ['立ちんぼ'], 'aaueone': ['笠舟本手'], 'ueooi': ['筆おろし'], 'iaaone': ['筏本手'], 'oin': ['粗チン'], 'uaa': ['素股', '素股'], 'euin': ['絶倫'], 'aioone': ['網代本手'], 'inau': ['緊縛', '陰核'], 'iueni': ['肉便器'], 'ueia': ['胸チラ'], 'aioi': ['脇コキ', '足コキ'], 'ii': ['自慰'], 'iuon': ['菊門'], 'aiooaai': ['蟻の戸渡り'], 'uaui': ['裏筋'], 'uaae': ['貝合わせ'], 'inuu': ['貧乳'], 'ininouan': ['近親相姦'], 'auaau': ['逆アナル'], 'aueiu': ['逆レイプ'], 'inei': ['陰茎'], 'inu': ['陰部'], 'aiaui': ['雁が首'], 'ena': ['電マ'], 'aoan': ['青姦'], 'ouun': ['食糞'], 'uiiieno': ['首引き恋慕'], 'ioui': ['騎乗位'], 'uuiuoaiaai': ['鶯の谷渡り'], 'ouonui': ['黄金水'], 'uoau': ['黒ギャル'], 'eueuuei': ['ＳＭプレイ'], 'eo': ['エロ']}
+    self.vowel2Ingo = {'uno':["うんこ"],'an':["マン"],'anii': ['3P'], 'eeuiouu': ['AV女優'], 'iiuoto': ['Gスポット'], 'eoae': ['NTR', '寝取られ'], 'etuu': ['SEX', 'せっくす', 'セックス'], 'eueu': ['SM'], 'euooii': ['SOD'], 'iiatu': ['Tバック'], 'iaaii': ['いやらしい'], 'eti': ['えっち', 'エッチ'], 'oinin': ['おちんちん'], 'otai': ['おっπ', 'おっぱい'], 'oaii': ['おなにー', 'オナニー'], 'oeoa': ['おねショタ'], 'ooo': ['おぼこ'], 'oano': ['おまんこ'], 'oeo': ['おめこ'], 'oouiea': ['お掃除フェラ'], 'inaa': ['きんたま', 'キンタマ', '金玉'], 'aaauuoi': ['さかさ椋鳥'], 'ioiuou': ['しぼり芙蓉'], 'uee': ['すけべ', 'スケベ'], 'eieione': ['せきれい本手'], 'aiuioouo': ['だいしゅきホールド'], 'ino': ['ちんこ', 'ちんぽ', 'インポ', 'チンコ', 'チンポ', '淫語'], 'inin': ['ちんちん', 'チンチン', '陰唇', 'ﾁﾝﾁﾝ'], 'ioieti': ['ひとりえっち', '一人Ｈ'], 'uaai': ['ふたなり'], 'anuiaei': ['まんぐり返し'], 'ano': ['まんこ', 'マンコ'], 'anan': ['まんまん'], 'uaua': ['むらむら', 'ムラムラ'], 'aue': ['アクメ'], 'aean': ['アゲマン', 'サゲマン'], 'aauoieo': ['アダルトビデオ'], 'aaii': ['アナニー'], 'aau': ['アナル'], 'aauetuu': ['アナルセックス', 'アナルＳＥＸ'], 'aauiiu': ['アナルビーズ'], 'aauuau': ['アナルプラグ'], 'aauauou': ['アナル拡張'], 'aauaiau': ['アナル開発'], 'aeao': ['アヘ顔'], 'iu': ['イク'], 'iiou': ['イチモツ'], 'iaiaetuu': ['イチャイチャセックス'], 'iaauetuu': ['イチャラブセックス'], 'ieua': ['イメクラ'], 'ieeiieo': ['イメージビデオ'], 'iaaio': ['イラマチオ'], 'inoenu': ['インポテンツ'], 'euuaii': ['エクスタシー'], 'io': ['痴女'], 'eoi': ['エロい', '寝取り', '手コキ'], 'eoouin': ['エロ同人'], 'eoouini': ['エロ同人誌'], 'eoon': ['エロ本'], 'oae': ['オナペ'], 'oaeto': ['オナペット'], 'oao': ['オナホ'], 'oaoou': ['オナホール'], 'ooauu': ['オーガズム'], 'auaa': ['カウパー'], 'anonouei': ['カントン包茎'], 'auoou': ['ギャグボール', 'ラブドール'], 'uuo': ['クスコ', '熟女'], 'uoai': ['クソガキ'], 'uioiu': ['クリトリス'], 'uniinuu': ['クンニリングス'], 'uni': ['クンニ'], 'euano': ['ケツマンコ'], 'onoou': ['コンドーム'], 'aaen': ['ザーメン'], 'ituuain': ['シックスナイン'], 'oaoe': ['ショタおね'], 'uaoo': ['スカトロ'], 'ueeiu': ['スケベ椅子'], 'ueua': ['スペルマ'], 'uatinu': ['スワッピング'], 'eue': ['セフレ'], 'enui': ['センズリ'], 'ouooneano': ['ソフト・オン・デマンド'], 'oouano': ['ソープランド'], 'oouou': ['ソープ嬢'], 'atiaiu': ['ダッチワイフ'], 'auuiiu': ['ダブルピース'], 'iuo': ['ディルド'], 'iiuuooo': ['ディープスロート'], 'eain': ['デカチン'], 'eiaiieuu': ['デリバリーヘルス'], 'eieu': ['デリヘル'], 'ooao': ['トロ顔'], 'ana': ['ナンパ', '顔射'], 'ooan': ['ノーパン'], 'aeoi': ['ハメ撮り'], 'aaeu': ['ハーレム'], 'aiaua': ['バイアグラ'], 'auuuea': ['バキュームフェラ'], 'aiui': ['パイズリ'], 'aian': ['パイパン', 'ヤリマン'], 'aaau': ['パパ活'], 'ania': ['パンチラ'], 'iti': ['ビッチ'], 'iuoatu': ['フィストファック'], 'ea': ['フェラ'], 'eaio': ['フェラチオ'], 'eaui': ['フェラ抜き'], 'uuea': ['ブルセラ'], 'etinu': ['ペッティング'], 'eian': ['ペニバン', '性感'], 'oeu': ['ホ別'], 'oeaa': ['ボテ腹'], 'ooin': ['ポコチン'], 'ouio': ['ポルチオ'], 'auaaeeon': ['マスターベーション'], 'aiin': ['ヤリチン', '催眠', '愛人'], 'auo': ['ラブホ'], 'auoeu': ['ラブホテル'], 'iue': ['リフレ'], 'eiu': ['レイプ'], 'oion': ['ロリコン'], 'aaai': ['中出し', '朝勃ち', '朝起ち'], 'ouai': ['乙π','おっぱい'], 'iaeoan': ['乱れ牡丹'], 'anou': ['乱交', '男娼'], 'iua': ['乳房'], 'iui': ['乳首'], 'itouiai': ['亀甲縛り'], 'iou': ['亀頭', '遅漏'], 'iaa': ['二穴'], 'iaaoui': ['二穴同時'], 'aeiouei': ['仮性包茎'], 'aii': ['体位'], 'oinauei': ['個人撮影'], 'auoaae': ['兜合わせ'], 'iiueone': ['入船本手'], 'enou': ['円光', '援交'], 'oo': ['処女'], 'ouei': ['包茎', '童貞'], 'ouaiaei': ['口内射精'], 'ouaiata': ['口内発射'], 'aauaiauu': ['唐草居茶臼'], 'aeioe': ['喘ぎ声'], 'iuuate': ['四十八手'], 'uooooi': ['太ももコキ'], 'ieaie': ['姫始め'], 'oiau': ['媚薬'], 'aaae': ['孕ませ', '玉舐め', '生ハメ'], 'oouione': ['寿本手'], 'aei': ['射精'], 'ian': ['屍姦'], 'ouu': ['巨乳'], 'oii': ['巨尻'], 'oon': ['巨根'], 'oaeauu': ['帆かけ茶臼'], 'ai': ['座位'], 'ouan': ['強姦', '睾丸'], 'ouaii': ['後背位'], 'iuu': ['微乳'], 'ioiiauu': ['忍び居茶臼'], 'aiauoi': ['快楽堕ち'], 'eiou': ['性交', '性欲'], 'eioi': ['性処理'], 'eioei': ['性奴隷'], 'eianataai': ['性感マッサージ'], 'eianai': ['性感帯'], 'eioui': ['性行為', '正常位'], 'aiu': ['愛撫'], 'aiei': ['愛液'], 'eiinue': ['成人向け'], 'aaniu': ['我慢汁'], 'ean': ['手マン'], 'uin': ['手淫'], 'aiiou': ['抱き地蔵'], 'aeaone': ['揚羽本手'], 'enoouai': ['援助交際'], 'ouou': ['放尿', '早漏', '陵辱'], 'ouiuei': ['放置プレイ'], 'iueauu': ['時雨茶臼'], 'uiiauu': ['月見茶臼'], 'auauui': ['松葉崩し'], 'aaoiauu': ['機織茶臼'], 'iuanuu': ['汁男優'], 'aieu': ['泡姫'], 'oaiione': ['洞入り本手'], 'inan': ['淫乱', '輪姦'], 'inou': ['淫行', '陰嚢', '陰毛', '飲尿'], 'ini': ['淫靡'], 'auuu': ['爆乳'], 'uuan': ['獣姦'], 'auou': ['発情'], 'ineiouei': ['真性包茎'], 'uian': ['睡姦'], 'aeue': ['種付け'], 'aeueueu': ['種付けプレス'], 'aaouai': ['穴兄弟'], 'aino': ['立ちんぼ'], 'aaueone': ['笠舟本手'], 'ueooi': ['筆おろし'], 'iaaone': ['筏本手'], 'oin': ['粗チン'], 'uaa': ['素股', '素股'], 'euin': ['絶倫'], 'aioone': ['網代本手'], 'inau': ['緊縛', '陰核'], 'iueni': ['肉便器'], 'ueia': ['胸チラ'], 'aioi': ['脇コキ', '足コキ'], 'ii': ['自慰'], 'iuon': ['菊門'], 'aiooaai': ['蟻の戸渡り'], 'uaui': ['裏筋'], 'uaae': ['貝合わせ'], 'inuu': ['貧乳'], 'ininouan': ['近親相姦'], 'auaau': ['逆アナル'], 'aueiu': ['逆レイプ'], 'inei': ['陰茎'], 'inu': ['陰部'], 'aiaui': ['雁が首'], 'ena': ['電マ'], 'aoan': ['青姦'], 'ouun': ['食糞'], 'uiiieno': ['首引き恋慕'], 'ioui': ['騎乗位'], 'uuiuoaiaai': ['鶯の谷渡り'], 'ouonui': ['黄金水'], 'uoau': ['黒ギャル'], 'eueuuei': ['ＳＭプレイ'], 'eo': ['エロ']}
     self.conversion = kakasi.getConverter()
     self.mecab = MeCab.Tagger()
     self.suuzi = [str(i) for i in range(0,10)]
+    for i in range(65296, 65296+10):
+      self.suuzi.append(chr(i))
     self.vowels = ["a","i","u","e","o"]
     self.new_vowels = ["a","i","u","e","o","t","n"]
+    self.alp = list(string.ascii_letters)
 
 
   def mecab_parse(self,user_input):
@@ -106,19 +110,27 @@ class AATG():
         word["trans"] = True
     return wordWithvowel
 
+  
+  def alp_check(self,word):
+    for w in word:
+      if w not in  self.alp:
+        return False
+    return True
+
 
   def make_input_vowels(self,parse_results):
     wordWithvowel= []
     #ローマ字変換
     for word in parse_results:
-      if word["pos"] == "名詞" and word["base"][0] not in self.suuzi:
-        result = {}
-        result["word"] = word["base"]
-        result["vowel"] = self.convert_vowels(word["base"])
-        result["surface"] = word["surface"]
-        result["trans"] = word["trans"]
-        wordWithvowel.append(result)
-   
+      if self.alp_check(word["base"]) == False:
+        if word["pos"] == "名詞" and word["base"][0] not in self.suuzi:
+          result = {}
+          result["word"] = word["base"]
+          result["vowel"] = self.convert_vowels(word["base"])
+          result["surface"] = word["surface"]
+          result["trans"] = word["trans"]
+          wordWithvowel.append(result)
+
     #母音のmask
     for converted in wordWithvowel:
       vowel = converted["vowel"]
@@ -189,6 +201,7 @@ class AATG():
               tmp = word["word"]
               tmp = tmp.replace(n_gram,random_choice,1)
               candidates[word["word"]].append(tmp)
+              print(candidates)
             del candidates[n_gram]
 
         if len(candidates[word["word"]]) == 0:
@@ -204,7 +217,7 @@ class AATG():
   def converted_output(self,parse_results,candidates):
     output = ""
     for word in parse_results:
-      if word["pos"] == "名詞" and word["base"][0] not in self.suuzi:
+      if word["pos"] == "名詞" and word["base"][0] not in self.suuzi and self.alp_check(word["base"]) == False:
         output += random.choice(candidates[word["base"]])
       else:
         output += word["surface"]
